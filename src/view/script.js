d0243182c14fd6e3b51d34fc7967a980032c37f1
Duy Khánh Nguyễn <ndkhanh1996@gmail.com>
2023-05-30 21:04:56 +0700

    add

--- src/view/script.js
+++ src/view/script.js
@@ -1,27 +1,27 @@
-let navbar = document.querySelector('.menu-btn');
+let menu = document.querySelector('.menu-btn');
 let cartItem = document.querySelector('.cart-items-container');
 let searchForm = document.querySelector('.search-form');
 
 document.querySelector('#menu-btn').onclick = () => {
-    navbar.classList.toggle('active');
+    menu.classList.toggle('active');
     searchForm.classList.remove('active');
     cartItem.classList.remove('active');
 }
 
 document.querySelector('#search-btn').onclick = () => {
     searchForm.classList.toggle('active');
-    navbar.classList.remove('active');
+    menu.classList.remove('active');
     cartItem.classList.remove('active');
 }
 
 document.querySelector('#cart-btn').onclick = () => {
     cartItem.classList.toggle('active');
-    navbar.classList.remove('active');
+    menu.classList.remove('active');
     searchForm.classList.remove('active');
 }
 
 window.onscroll = () => {
-    navbar.classList.remove('active');
+    menu.classList.remove('active');
     searchForm.classList.remove('active');
     cartItem.classList.remove('active');
 }
