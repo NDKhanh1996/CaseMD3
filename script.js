5fb9bfd148cfc1754aa06d70842d0eb3f429037c
Nguyễn Duy Khánh <khanh@MacBook-Air-cua-Nguyen.local>
2023-05-30 16:35:23 +0700

    add

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
-let navbar = document.querySelector('.navbar');
+let navbar = document.querySelector('.menu-btn');
+let cartItem = document.querySelector('.cart-items-container');
+let searchForm = document.querySelector('.search-form');
 
 document.querySelector('#menu-btn').onclick = () => {
+    // cartItem.classList.toggle('active');
     navbar.classList.toggle('active');
     searchForm.classList.remove('active');
     cartItem.classList.remove('active');
 }
-let searchForm = document.querySelector('.search-form');
 
 document.querySelector('#search-btn').onclick = () => {
+    console.log('search-btn');
     searchForm.classList.toggle('active');
     navbar.classList.remove('active');
     cartItem.classList.remove('active');
 }
-let cartItem = document.querySelector('.cart-items-container');
+
 
 document.querySelector('#cart-btn').onclick = () => {
+    console.log('cart-btn');
     cartItem.classList.toggle('active');
     navbar.classList.remove('active');
     searchForm.classList.remove('active');
