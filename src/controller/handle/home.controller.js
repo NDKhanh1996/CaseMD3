80ef347ddf91971a3e85bf185b032df1d421e9ef
Nguyễn Duy Khánh <khanh@MacBook-Air-cua-Nguyen.local>
2023-06-01 11:40:08 +0700

    fix url to index in home.controller

--- src/controller/handle/home.controller.js
+++ src/controller/handle/home.controller.js
@@ -1,7 +1,7 @@
 const BaseController = require('./base.controller');
 class HomeController{
     async getHomePage (req, res){
-        let html = await BaseController.getTemplate('./src/view/home/index.html');
+        let html = await BaseController.getTemplate('./src/view/index.html');
         res.writeHead(200,{'Content-Type':'text/html'});
         res.write(html);
         res.end();
