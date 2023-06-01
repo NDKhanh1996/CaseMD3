a37d048a502c756f31b8eab8ad8b3751845300fa
CaoMinhThuan <caominhthuan2506@gmail.com>
2023-06-01 10:16:12 +0700

    add base and database

---
+++ src/model/base-model.js
@@ -0,0 +1,18 @@
+const database = require('./database-model');
+class BaseModel {
+    constructor() {
+        this.connect = database.connect()
+    };
+    querySql(sql) {
+        return new Promise((resolve, reject) => {
+            this.connect.query(sql, (err, data) => {
+                if (err) {
+                    reject(err.mesaage)
+                }
+                resolve(data)
+            })
+                
+            
+        })
+    }
+}
